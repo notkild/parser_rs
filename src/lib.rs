34453e0c883cea6633486f93a289e28acc6c6120
notkild <notkild@gmail.com>
2016-10-19 22:10:08 +0200

    Add consume_until and consume_until_str function

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,11 @@ impl Parser {
         self.idx >= self.content.len()
     }
 
+    /// Check if the content starts with the specified str
+    pub fn starts_with(&self, s: &str) -> bool {
+        self.content[self.idx..].starts_with(s)
+    }
+
     /// Return the next char without changing the index
     pub fn next(&self) -> char {
         self.content[self.idx..].chars().next().unwrap()
@@ -36,6 +41,21 @@ impl Parser {
     {
         let mut result = String::new();
         while !self.is_eof() && func(self.next()) {
+            let c = self.consume();
+            result.push(c);
+        }
+        result
+    }
+
+    /// Consume until the next char is the specified char
+    pub fn consume_until(&mut self, c: char) -> String {
+        self.consume_while(|nc| nc != c)
+    }
+
+    /// Consume until the content match the specified str
+    pub fn consume_until_str(&mut self, s: &str) -> String {
+        let mut result = String::new();
+        while !self.is_eof() && !self.starts_with(s) {
             let c = self.consume();
             result.push(c);
         }
--- tests/lib.rs
+++ tests/lib.rs
@@ -17,3 +17,19 @@ fn consume_while() {
     let mut parser = Parser::new(s);
     assert_eq!(parser.consume_while(|c| c != ' '), "hello");
 }
+
+#[test]
+fn consume_until() {
+    let s = "hello world from another world";
+    let mut parser = Parser::new(s);
+    assert_eq!(parser.consume_until('r'), "hello wo");
+    parser.consume();
+    assert_eq!(parser.consume_until('r'), "ld f");
+}
+
+#[test]
+fn consume_until_str() {
+    let s = "hello world from another world";
+    let mut parser = Parser::new(s);
+    assert_eq!(parser.consume_until_str("another"), "hello world from ");
+}
